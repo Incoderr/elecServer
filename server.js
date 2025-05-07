706e817e6cdcbb2c63cd1888a94e48f5a3114385
Incoderr <laenartem75@gmail.com>
2025-05-07 20:37:22 +0500

    web

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'https://elec-app.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://elec-app.vercel.app'],
     methods: ['GET', 'POST']
-  }
+  },
+  transports: ['websocket', 'polling']
 });
-
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://elec-app.vercel.app'],
+  methods: ['GET', 'POST']
+}));
 // Store connected users
 const users = new Map();
 
