30109fdfb1a28da5b67ea867a6db517d95fca70f
incoder laptop <laenartem75@gmail.com>
2025-10-04 17:19:58 +0500

    server

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
-const app = require('./app');
-const { initSocket } = require('./socket');
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
-const PORT = process.env.PORT || 3000;
-
-const server = app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: '*', // Разрешить все origins для разработки; в продакшене укажите ваш домен
+    methods: ['GET', 'POST']
+  }
 });
 
-initSocket(server);
+io.on('connection', (socket) => {
+  console.log('Пользователь подключился:', socket.id);
+
+  // Обработка получения сообщения от клиента
+  socket.on('chat message', (msg) => {
+    console.log('Сообщение:', msg);
+    io.emit('chat message', msg); // Отправить всем подключенным клиентам
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Пользователь отключился:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
